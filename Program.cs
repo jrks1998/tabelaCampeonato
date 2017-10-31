c3d3588f9b33870a2f7b168a9c8eb8f10f5465b1
jrks1998 <jonas.schneider@institutoivoti.com.br>
2017-10-31 21:19:21 -0200

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tabelaCampeonato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
